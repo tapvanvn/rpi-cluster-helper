866ba400abd99b3a0800b0a7830dc324050f51bc
Duy Nguyen <tapvanvn@gmail.com>
2021-07-30 12:34:34 +0700

    update

--- rpi-cluster/setup_ws_cifs.sh
+++ rpi-cluster/setup_ws_cifs.sh
@@ -3,3 +3,7 @@ mkdir -p "$VOLUME_PLUGIN_DIR/fstab~cifs"
 cd "$VOLUME_PLUGIN_DIR/fstab~cifs"
 curl -L -O https://raw.githubusercontent.com/fstab/cifs/master/cifs
 chmod 755 cifs
+
+sudo apt install -y jq
+
+$VOLUME_PLUGIN_DIR/fstab~cifs/cifs init
